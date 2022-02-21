6a387e2bfab28bea60abb7bf4522ec2a900e6570
Marina Tsymlianskaia <marinatsy16@gmail.com>
2022-02-21 08:47:02 -0700

    * .gitignore: first commit * ClassCar.sln: * ClassCar/Car.cs: * ClassCar/Program.cs: * ClassCar/ClassCar.csproj:

---
+++ ClassCar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "4Runner";
+            myCar.Year = 2020;
+
+            Console.WriteLine($"My Car Make is {myCar.Make}.");
+            Console.WriteLine($"My Car Model is {myCar.Model}.");
+            Console.WriteLine($"My Car Year is {myCar.Year}.");
+
+        }
+    }
+}
